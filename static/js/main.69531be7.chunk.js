1fe6d5d38c0d148f1326a41783cded4df24b345c
Achilleus190612 <dennislee190612@gmail.com>
2025-07-05 05:19:41 +0200

    Update main.69531be7.chunk.js

--- static/js/main.69531be7.chunk.js
+++ static/js/main.69531be7.chunk.js
@@ -854,7 +854,7 @@
                                                             height: "20px",
                                                             src: N,
                                                             alt: ""
-                                                        }), " ", "EYC"]
+                                                        }), " ", "PARA"]
                                                     })]
                                                 })]
                                             }), Object(h.jsx)(u.a, {
